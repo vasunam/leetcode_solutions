40ba3c7f90ae259d8e212ea1395bed2e28531fa3
vasunam <vasunamratha@gmail.com>
2020-05-25 21:44:23 +0530

    Monotonic Array Q896

---
+++ 896.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isMonotonic(self, A: List[int]) -> bool:
+        inc=True
+        dec=True
+     
+        for i in range(len(A)-1):
+            if A[i]>A[i+1]:
+                inc= False
+                
+            if A[i]<A[i+1]:
+                dec= False
+  
+        return inc or dec
