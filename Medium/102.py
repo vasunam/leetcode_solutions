0b4675a70b7cb9a3dd7ba8419e3c8c6e3a1c1416
vasunam <vasunamratha@gmail.com>
2020-06-04 18:03:22 +0530

    Binary Tree Level Order Traversal  Q102

---
+++ Medium/102.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def printLevel(self, root, height, result):
+        if height==0:
+            result.append(root.val)
+        if root.left:
+            self.printLevel(root.left, height-1, result)
+        if root.right:
+            self.printLevel(root.right, height-1, result)
+        
+    def findHeight(self, root):
+        if root==None:
+            return 1
+        return 1+max(self.findHeight(root.left),self.findHeight(root.right))
+        
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        result=[]
+        
+        if root==None:
+            return result
+        
+        height=self.findHeight(root)-1
+        for i in range(0,height):
+            level=[]
+            self.printLevel(root, i, level)
+            result.append(level)
+            
+        return result
